6d5f5c61d417dbb6f5feea9c0b4359a60608d257
Michael Goddard <mgoddard@gopivotal.com>
2017-09-08 05:46:38 -0400

    Pulling Cmake over HTTPS

--- 05_install_cmake.sh
+++ 05_install_cmake.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-tar xzvf cmake-3.9.1.tar.gz 
-cd cmake-3.9.1/
-./configure 
+cmake="cmake-3.9.2"
+
+curl https://cmake.org/files/v3.9/$cmake.tar.gz | tar xzvf -
+cd $cmake/
+./configure
 make
 sudo make install
 cd ..
